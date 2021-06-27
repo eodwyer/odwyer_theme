de47e931f25aa0faef9427a73dee92a3d45aeb86
eodwyer <eoin.odwyer9@gmail.com>
2021-06-26 20:40:59 -0700

    Switch to spaces and remove jetpack

--- footer.php
+++ footer.php
@@ -11,21 +11,21 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'odwyer' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'odwyer' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'odwyer' ), 'odwyer', '<a href="http://eoinodwyer.com">Eoin O\'Dwyer</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+ <footer id="colophon" class="site-footer">
+  <div class="site-info">
+   <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'odwyer' ) ); ?>">
+    <?php
+    /* translators: %s: CMS name, i.e. WordPress. */
+    printf( esc_html__( 'Proudly powered by %s', 'odwyer' ), 'WordPress' );
+    ?>
+   </a>
+   <span class="sep"> | </span>
+    <?php
+    /* translators: 1: Theme name, 2: Theme author. */
+    printf( esc_html__( 'Theme: %1$s by %2$s.', 'odwyer' ), 'odwyer', '<a href="http://eoinodwyer.com">Eoin O\'Dwyer</a>' );
+    ?>
+  </div><!-- .site-info -->
+ </footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
--- inc/customizer.php
+++ inc/customizer.php
@@ -11,26 +11,26 @@
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
 function odwyer_customize_register( $wp_customize ) {
-	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
-	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+ $wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+ $wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+ $wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
 
-	if ( isset( $wp_customize->selective_refresh ) ) {
-		$wp_customize->selective_refresh->add_partial(
-			'blogname',
-			array(
-				'selector'        => '.site-title a',
-				'render_callback' => 'odwyer_customize_partial_blogname',
-			)
-		);
-		$wp_customize->selective_refresh->add_partial(
-			'blogdescription',
-			array(
-				'selector'        => '.site-description',
-				'render_callback' => 'odwyer_customize_partial_blogdescription',
-			)
-		);
-	}
+ if ( isset( $wp_customize->selective_refresh ) ) {
+  $wp_customize->selective_refresh->add_partial(
+   'blogname',
+   array(
+    'selector'        => '.site-title a',
+    'render_callback' => 'odwyer_customize_partial_blogname',
+   )
+  );
+  $wp_customize->selective_refresh->add_partial(
+   'blogdescription',
+   array(
+    'selector'        => '.site-description',
+    'render_callback' => 'odwyer_customize_partial_blogdescription',
+   )
+  );
+ }
 }
 add_action( 'customize_register', 'odwyer_customize_register' );
 
@@ -40,7 +40,7 @@ add_action( 'customize_register', 'odwyer_customize_register' );
  * @return void
  */
 function odwyer_customize_partial_blogname() {
-	bloginfo( 'name' );
+ bloginfo( 'name' );
 }
 
 /**
@@ -49,13 +49,13 @@ function odwyer_customize_partial_blogname() {
  * @return void
  */
 function odwyer_customize_partial_blogdescription() {
-	bloginfo( 'description' );
+ bloginfo( 'description' );
 }
 
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function odwyer_customize_preview_js() {
-	wp_enqueue_script( 'odwyer-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+ wp_enqueue_script( 'odwyer-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
 }
 add_action( 'customize_preview_init', 'odwyer_customize_preview_js' );
