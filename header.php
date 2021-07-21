24362736b0d2b8f2c4386f5384bd68b8da5f8efc
eodwyer <eoin.odwyer9@gmail.com>
2021-07-20 17:13:40 -0700

    Fix linting issues for scss

--- header.php
+++ header.php
@@ -27,42 +27,39 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-  <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'odwyer' ); ?></a>
+<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'odwyer' ); ?></a>
 
-  <?php if(!is_page_template("homepage.php")): ?>
-    <header id="masthead" class="site-header">
-      <div class="site-branding">
-        <?php
-        the_custom_logo();
-        ?>
-        <div class="site-branding__text">
+<?php if(!is_page_template("homepage.php")): ?>
+ <header id="masthead" class="site-header">
+  <div class="site-branding">
+   <?php
+   the_custom_logo();
+   ?>
+   <div class="site-branding__text">
         <?php if ( is_front_page() || is_home() ) : ?>
-            <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-        <?php
-          else :
-            ?>
-            <p class="h1 site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-            <?php
-          endif;
-          $odwyer_description = get_bloginfo( 'description', 'display' );
-          if ( $odwyer_description || is_customize_preview() ) :
-            ?>
-            <p class="site-description"><?php echo $odwyer_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-        <?php endif; ?>
-        </div><!-- .site-branding__text -->
-      </div><!-- .site-branding -->
+     <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+        <?php else : ?>
+     <p class="h1 site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+    <?php 
+        endif;
+    $odwyer_description = get_bloginfo( 'description', 'display' );
+    if ( $odwyer_description || is_customize_preview() ) : ?>
+      <p class="site-description"><?php echo $odwyer_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+       <?php endif; ?>
+   </div><!-- .site-branding__text -->
+  </div><!-- .site-branding -->
 
-      <nav id="site-navigation" class="main-navigation">
-        <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'odwyer' ); ?></button>
-        <?php
-        wp_nav_menu(
-          array(
-            'theme_location' => 'menu-1',
-            'menu_id'        => 'primary-menu',
-            'walker' => new icon_walker()
-          )
-        );
-        ?>
-      </nav><!-- #site-navigation -->
-    </header><!-- #masthead -->
-  <?php endif; ?>
+  <nav id="site-navigation" class="main-navigation">
+   <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'odwyer' ); ?></button>
+   <?php
+   wp_nav_menu(
+     array(
+       'theme_location' => 'menu-1',
+       'menu_id'        => 'primary-menu',
+       'walker' => new icon_walker()
+     )
+   );
+   ?>
+  </nav><!-- #site-navigation -->
+ </header><!-- #masthead -->
+<?php endif; ?>
